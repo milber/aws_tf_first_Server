be4881bfa4775ec4a46ed22e0ce0b8fbe7606725
Milber Champutiz <milber.champutiz@thoughtworks.com>
2023-06-15 15:58:39 -0500

    add laod balancer

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "milber-infra-squad-sg" {
     name = "milber-infra-squad-sg"
 
     ingress {
-      cidr_blocks = ["0.0.0.0/0"]
+      security_groups = [aws_security_group.milber_alb_sg.id]
       description = "Access to 8080 form exterior"
       from_port = 8080
       to_port = 8080
@@ -66,3 +66,70 @@ resource "aws_instance" "milber_ec2_infra_squad-02" {
               EOF
 
 }
+
+resource "aws_security_group" "milber_alb_sg" {
+    name = "milber_alb_sg"
+
+    ingress {
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Access to 80 form exterior"
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+    }
+
+    egress {
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Access to 80 of internal servers"
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+    }
+
+}
+
+resource "aws_lb" "milber_app_lb" {
+  load_balancer_type = "application"
+  name = "milber-Lb"
+  security_groups = [aws_security_group.milber_alb_sg.id]
+  subnets = [data.aws_subnet.az_a.id, data.aws_subnet.az_b.id]
+}
+
+resource  "aws_lb_target_group" "milber_is_tg" {
+  name = "milber-is-tg"
+  port = 80
+  vpc_id = aws_security_group.milber-infra-squad-sg.vpc_id
+  protocol = "HTTP"
+
+  health_check {
+    enabled = true
+    matcher = 200
+    path    = "/"
+    port    = 8080
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "servidor_1" {
+  target_group_arn  = aws_lb_target_group.milber_is_tg.arn
+  target_id         = aws_instance.milber_ec2_infra_squad-01.id
+  port              = 8080
+}
+
+resource "aws_lb_target_group_attachment" "servidor_2" {
+  target_group_arn  = aws_lb_target_group.milber_is_tg.arn
+  target_id         = aws_instance.milber_ec2_infra_squad-02.id
+  port              = 8080
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.milber_app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn  = aws_lb_target_group.milber_is_tg.arn
+    type              = "forward"
+  } 
+}
+
--- output.tf
+++ output.tf
@@ -7,3 +7,9 @@ output "public_dns_02" {
   description = "Public dns"
   value = "http://${aws_instance.milber_ec2_infra_squad-02.public_dns}:8080"
 }
+
+output "dns_load_balancer" {
+  description = "Public dns"
+  value = "http://${aws_lb.milber_app_lb.dns_name}:80"
+}
+
